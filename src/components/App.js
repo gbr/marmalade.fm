8297401b66d1cd7360e0d17671a49ad49ea94227
Glenn Ryan <glennthepen@gmail.com>
2019-05-30 19:26:29 -0700

    Finished setting up and styling basic components

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Marmalade.fm</h1>
-      </header>
-    </div>
-  );
+import FeaturedMix from './FeaturedMix';
+import Header from './Header';
+
+const Home = () => <h1>Home</h1>
+const Archive = () => <h1>Archive</h1>
+const About = () => <h1>About</h1>
+
+class App extends Component {
+  render() {
+    return (
+      // router wraps our whole page and lets us use react-router
+      <Router>
+        {/* outermost container */}
+        <div>
+          {/* page container */}
+          <div className='flex-l justify-end'>
+            <FeaturedMix />
+            <div className="w-50-l relative z-1">
+              <Header />
+              {/* Routed page */}
+              <Route exact path="/" component={Home} />
+              <Route path="/archive" component={Archive} />
+              <Route path="/about" component={About} />
+            </div>
+          </div>
+          {/* AudioPlayer */}
+          <iframe width="100%" height="60" src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=%2FNTSRadio%2Fradio-ghibli-part-1-1986-1992-17th-january-2017%2F" frameBorder="0" className="player db fixed bottom-0 z-5" />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = props => (
+    <div className="black mb5 pt5">
+        <h1 className="ttu f3 tracked-mega anton tc mt0 mb3">Marmalade.fm</h1>
+        <ul className="list flex justify-center pl0">
+            <li className="mh2">
+                <NavLink exact to="/" href="#" className="nav-link link biryani-black f6 ttu gray">What's hot</NavLink>
+            </li>
+            <li className="mh2">
+                <NavLink to="/archive" href="#" className="nav-link link biryani-black f6 ttu gray">About</NavLink>
+            </li>
+            <li className="mh2">
+                <NavLink to="/about" href="#" className="nav-link link biryani-black f6 ttu gray">Archive</NavLink>
+            </li>
+        </ul>
+    </div>
+)
+
+export default Header;
