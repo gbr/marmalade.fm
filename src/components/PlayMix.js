ca36361639549e26204e710b570d8508b8d144c7
Glenn Ryan <glennthepen@gmail.com>
2019-07-02 14:29:44 -0700

    Made final changes to CSS and hooked up the classNames in PlayMix to properly reflect play state. However, the "playing" component is missing from Redux and React state for some reason. Waiting untitl nearer the final code to troubleshoot if still a problem.

--- src/components/PlayMix.js
+++ src/components/PlayMix.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import classNames from 'classnames'
 
 import actions from '../store/actions'
 
 // this component wraps around anything
 // we want to play from click events
 // it purely provides functionality—no visual styling
-function PlayMix({ playMix, playing, id, currentMix, children, className }) {
+function PlayMix({ playMix, playing, id, currentMix, children, className, fromMixCloud }) {
     return (
-        <div className={`${className} ${id === currentMix && playing && 'playing'}`}
+        <div
+            className={
+                classNames({
+                    [className]: className,
+                    playing: id === currentMix && playing && fromMixCloud,
+                    loading: id === currentMix && !playing && !fromMixCloud
+                })
+            }
             onClick={() => playMix({ currentMix: id, fromMixCloud: false })}>
             {children}
         </div>
