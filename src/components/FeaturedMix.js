9f5755b5425b194d6fc0d09fa64a31b2cf379edf
Glenn Ryan <glennthepen@gmail.com>
2019-07-02 17:02:03 -0700

    Added a new selector that changes the text in the left pane depending on what we're doing

--- src/components/FeaturedMix.js
+++ src/components/FeaturedMix.js
@@ -4,11 +4,11 @@ import { connect } from 'react-redux'
 import PlayButton from './PlayButton';
 import PlayMix from './PlayMix';
 
-const FeaturedMix = ({ name, pictures = {}, ...props }) => (
+const FeaturedMix = ({ name, pictures = {}, title, ...props }) => (
     <div className='w-50-l vh-100 flex items-center justify-center cover bg-center bg-featured pad-bottom fixed-l left-0 mix-overlay' style={{ backgroundImage: `url(${pictures.extra_large})` }}>
         <PlayMix {...props} >
             <div className="w-100 tc pa3 relative z-2">
-                <p className="b biryani f6 white ttu">Featured Mix</p>
+                <p className="b biryani f6 white ttu">{title}</p>
                 <h1 className="mix-title mt0 mb3 anton white ttu">{name}</h1>
                 <PlayButton />
             </div>
@@ -43,6 +43,17 @@ const getMix = (state) => {
     return featuredMix || firstMix;
 }
 
+const getTitle = state => {
+    if (state.featuredMix) {
+        return 'Currently viewing'
+    } else if (state.currentMix && state.playing) {
+        return 'Currently playing'
+    } else {
+        return 'Featured mix'
+    }
+}
+
 export default connect(state => ({
-    ...getMix(state)
+    ...getMix(state),
+    title: getTitle(state)
 }))(FeaturedMix)
